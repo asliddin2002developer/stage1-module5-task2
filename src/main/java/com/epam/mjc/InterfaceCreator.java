1cdea5ffe30161cce2bd075250a313efe86ba5de
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-22 11:50:18 +0500

    Lambda Task Done

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,16 @@
 package com.epam.mjc;
 
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class InterfaceCreator {
 
+
+
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Function<Integer, Integer> devide = integer -> integer / divider;
+        return operation -> operation.stream()
+                                    .map(devide)
+                                    .collect(Collectors.toList());
     }
 }
